4861080a43e7f3554d5e35faf5134c916c4e10bf
aki-masaki <nikistefan95@gmail.com>
2025-12-11 21:37:41 +0200

    project

---
+++ README.md
@@ -0,0 +1,17 @@
+# nvim-virtual-colors
+
+this plugin displays a colored block character before a **hex** color in `.css` files.
+
+## installing
+
+with Plug:
+```lua
+Plug 'aki-masaki/nvim-virtual-colors'
+```
+
+## usage
+
+for the plugin to work, you have to `require` it in your `init.lua`:
+```lua
+require('nvim-virtual-colors').setup{}
+```
---
+++ lua/nvim-virtual-colors.lua
@@ -0,0 +1,86 @@
+local M = {
+  opts = {},
+  ns_id = 0,
+}
+
+function M.init_autocmd()
+  vim.api.nvim_create_autocmd('InsertEnter', {
+    pattern = {'*.css'},
+    callback = function(ev)
+      vim.api.nvim_buf_clear_namespace(0, M.ns_id, 0, -1)
+    end
+  })
+
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    pattern = {'*.css'},
+    callback = function(ev)
+      M.detect_colors()
+    end
+  })
+
+  vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = {'*.css'},
+    callback = function(ev)
+      M.detect_colors()
+    end
+  })
+end
+
+function M.detect_colors()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local color_index
+  local semic_index
+  local color
+  local len
+  local offset = 0
+
+  for i, line in ipairs(lines) do
+    while true do
+      color_index = line:find('#', offset)
+      semic_index = line:find(';', offset)
+
+      if color_index ~= nil and semic_index ~= nil then
+        len = semic_index - color_index - 1
+
+        if not (len == 3 or len == 6) then
+          return
+        end
+
+        len = semic_index - color_index - 1
+
+        color = line:sub(color_index + 1, semic_index - 1)
+
+        if len == 3 then
+          color = color:rep(2)
+        end
+
+        vim.api.nvim_set_hl(0, color, { fg = '#' .. color })
+
+        local opts = {
+          virt_text = {{M.block_character, color}},
+          virt_text_pos = 'inline',
+        }
+
+        local mark_id = vim.api.nvim_buf_set_extmark(0, M.ns_id, i - 1, color_index - 1, opts)
+
+        offset = offset + semic_index + 1
+      else
+        offset = 0
+
+        break
+      end
+    end
+  end
+end
+
+function M.setup(opts)
+  M.opts = opts or {
+    block_character = '█',
+  }
+
+  M.ns_id = vim.api.nvim_create_namespace("virtual-colors")
+
+  M.init_autocmd()
+end
+
+return M
