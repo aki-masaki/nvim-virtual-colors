fcda647c9de2819500782a792bc02ebc9fded8e3
aki-masaki <nikistefan95@gmail.com>
2025-12-12 21:00:42 +0200

    feat: added support for background display

--- lua/nvim-virtual-colors.lua
+++ lua/nvim-virtual-colors.lua
@@ -1,4 +1,5 @@
 local M = {
+  opts = {},
   ns_id = nil,
 }
 
@@ -52,12 +53,42 @@ function M.detect_colors()
           color = color:rep(2)
         end
 
-        vim.api.nvim_set_hl(0, color, { fg = '#' .. color })
+        local red   = tonumber(color:sub(1, 2), 16)
+        local green = tonumber(color:sub(3, 4), 16)
+        local blue  = tonumber(color:sub(5, 6), 16)
 
-        local opts = {
-          virt_text = {{'█', color}},
-          virt_text_pos = 'inline',
-        }
+        local opts
+
+        if M.opts.display == 'bg' or M.opts.display == 'bg-fn' then
+          local fg
+
+          if red + green + blue < 200 then
+            fg = "#ffffff"
+          else
+            fg = "#000000"
+          end
+
+          vim.api.nvim_set_hl(0, color, { bg = '#' .. color , fg = fg})
+
+          if M.opts.display == 'bg' then
+            opts = {
+              virt_text = {{'#' .. color, color}},
+              virt_text_pos = 'overlay',
+            }
+          else
+            opts = {
+              virt_text = {{'#', color}},
+              virt_text_pos = 'overlay',
+            }
+          end
+        else
+          vim.api.nvim_set_hl(0, color, { fg = '#' .. color})
+
+          opts = {
+            virt_text = {{'█', color}},
+            virt_text_pos = 'inline',
+          }
+        end
 
         local mark_id = vim.api.nvim_buf_set_extmark(0, M.ns_id, i - 1, color_index - 1, opts)
 
@@ -86,18 +117,43 @@ function M.detect_colors()
         local green = tonumber(line:sub(comma1 + 1, comma2 - 1))
         local blue = tonumber(line:sub(comma2 + 1, rpar - 1))
 
-        if red == nil or green == nil or blue == nil then
+        if red == nil or red < 0 or red > 255 or green == nil or green < 0 or green > 255 or blue == nil or blue < 0 or blue > 255 then
           break
         end
 
         local color = string.format("%02x%02x%02x", red, green, blue)
 
-        local opts = {
-          virt_text = {{'█', color}},
-          virt_text_pos = 'inline',
-        }
+        if M.opts.display == 'bg' or M.opts.display == 'bg-fn' then
+          local fg
 
-        vim.api.nvim_set_hl(0, color, { fg = '#' .. color })
+          if red + green + blue < 200 then
+            fg = "#ffffff"
+          else
+            fg = "#000000"
+          end
+
+          vim.api.nvim_set_hl(0, color, { bg = '#' .. color , fg = fg})
+
+          if M.opts.display == 'bg' then
+            opts = {
+              virt_text = {{string.format("rgb(%d, %d, %d)", red, green, blue), color}},
+              virt_text_pos = 'overlay',
+            }
+          else
+            opts = {
+              virt_text = {{'rgb', color}},
+              virt_text_pos = 'overlay',
+            }
+          end
+        else
+          vim.api.nvim_set_hl(0, color, { fg = '#' .. color})
+
+          opts = {
+            virt_text = {{'█', color}},
+            virt_text_pos = 'inline',
+          }
+        end
+
 
         local mark_id = vim.api.nvim_buf_set_extmark(0, M.ns_id, i - 1, rgb_index - 1, opts)
 
@@ -112,6 +168,12 @@ function M.detect_colors()
 end
 
 function M.setup(opts)
+  opts = opts or {}
+
+  M.opts = {
+    display = opts.display or 'bg',
+  }
+
   M.ns_id = vim.api.nvim_create_namespace("virtual-colors")
 
   M.init_autocmd()
